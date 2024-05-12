0c6b6c37cdd35446fc2549df011f2e5d3f15a90e
nivedTrigeminal <nivedram@trigeminal-ai.com>
2024-05-12 13:53:58 +0530

    Initial Push for portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,270 @@
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  docker,
+  postgresql,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+  github,
+  mongo,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Frontend Developer",
+    icon: frontend,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "UI/UX Design",
+    icon: ux,
+  },
+  {
+    title: "Project Management",
+    icon: prototyping,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "GitHub",
+    icon: github,
+  },
+  {
+    name: "MongoDB",
+    icon: mongo,
+  },
+  {
+    name: "postgresql",
+    icon: postgresql,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  // {
+  //   title: "Front-End Developer",
+  //   company_name: "Cover Hunt",
+  //   icon: coverhunt,
+  //   iconBg: "#333333",
+  //   date: "Aug 2021 - Feb 2022",
+  // },
+  {
+    title: "MEARN Stack intern",
+    company_name: "Luminar Technolab",
+    icon: microverse,
+    iconBg: "#fff",
+    date: "Mar 2022 - May 2022",
+  },
+  {
+    title: "Junior Software Engineer",
+    company_name: "BJeLT Technologies",
+    icon: kelhel,
+    iconBg: "#fff",
+    date: "May 2022 - Oct 2022",
+  },
+  {
+    title: "Full Stack Developer",
+    company_name: "Trigeminal ai",
+    icon: dcc,
+    iconBg: "#fff",
+    date: "Sep 2022 - Present",
+  },
+];
+
+const projects = [
+  {
+    id: "project-1",
+    name: "RestoAp",
+    description: "A complete restaurent management app with QR code odering",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: komikult,
+    // repo: "https://github.com/shaqdeff/KomiKult",
+    // demo: "https://shaqdeff.github.io/KomiKult/",
+  },
+  {
+    id: "project-2",
+    name: "Sociopedia",
+    description: "A social media application using MERN stack.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: leaderboard,
+    // repo: "https://github.com/shaqdeff/Leaderboard",
+    // demo: "https://shaqdeff.github.io/Leaderboard/",
+  },
+  {
+    id: "project-3",
+    name: "Netflix clone",
+    description: "This is a netflix clone made using react redux",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: math,
+    // repo: "https://github.com/shaqdeff/Math-Magicians",
+    // demo: "https://inspiring-medovik-37d3b3.netlify.app/",
+  },
+  {
+    id: "project-4",
+    name: "EKart",
+    description: `An online shopping application using react redux`,
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: movie,
+    repo: "https://github.com/shaqdeff/Movie-Metro",
+    demo: "https://movie-metro.netlify.app/",
+  },
+  // {
+  //   id: "project-5",
+  //   name: "Nyeusi Fest Site",
+  //   description:
+  //     "This is a demo concert website for a music festival called Nyeusi.",
+  //   tags: [
+  //     {
+  //       name: "nextjs",
+  //       color: "blue-text-gradient",
+  //     },
+  //     {
+  //       name: "supabase",
+  //       color: "green-text-gradient",
+  //     },
+  //     {
+  //       name: "css",
+  //       color: "pink-text-gradient",
+  //     },
+  //   ],
+  //   image: nyeusi,
+  //   repo: "https://github.com/shaqdeff/Nyeusi-Fest-Site",
+  //   demo: "https://shaqdeff.github.io/Nyeusi-Fest-Site/",
+  // },
+];
+
+export { services, technologies, experiences, projects };
